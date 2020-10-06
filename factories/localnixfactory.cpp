3a6bdfbcff33962640c6af8f1bd6dda3b9b8b3e4
cxom4uk <cxom4uk@gmail.com>
2020-10-07 01:07:13 +0300

    Add getProcessList to LocalNixFactory

--- factories/localnixfactory.cpp
+++ factories/localnixfactory.cpp
@@ -1,6 +1,20 @@
 #include "localnixfactory.h"
 
-LocalNixFactory::LocalNixFactory(QObject *parent) : QObject(parent)
+easy::LocalNixFactory::LocalNixFactory(QObject *parent) : BaseFactory(parent)
 {
 
+}
+
+easy::ProcessList easy::LocalNixFactory::getProcessList() const
+{
+    QDir directory(
+                "/proc",
+                "[1-9][0-9]*",
+                QDir::NoSort,
+                QDir::Dirs | QDir::NoSymLinks
+                );
+
+    ProcessList result = directory.entryList();
+
+    return result;
 }
--- factories/localnixfactory.h
+++ factories/localnixfactory.h
@@ -3,14 +3,24 @@
 
 #include <QObject>
 
-class LocalNixFactory : public QObject
+#include <QDir>
+
+#include <QString>
+
+#include "basefactory.h"
+
+namespace easy {
+
+class LocalNixFactory : public BaseFactory
 {
     Q_OBJECT
 public:
     explicit LocalNixFactory(QObject *parent = nullptr);
 
-signals:
+    ProcessList getProcessList() const override;
 
 };
+
+}
 
 #endif // LOCALNIXFACTORY_H
